347329d9ad7fc53ec85880e4ff7fc702f4dfd208
Claudio Corridore <claudio.corridore@gmail.com>
2026-08-16 11:30:15 +0100

    feat(custom): set C spacing to align with clang

--- lua/custom/autocommands.lua
+++ lua/custom/autocommands.lua
@@ -8,3 +8,17 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.opt_local.spelllang = 'en_us'
   end,
 })
+
+-- Set 2 spaces indentation for C / C++ files to match autoformat (clang-format)
+local c_indent_group = vim.api.nvim_create_augroup('c-indentation', { clear = true })
+vim.api.nvim_create_autocmd('FileType', {
+  group = c_indent_group,
+  pattern = { 'c', 'cpp' },
+  callback = function()
+    vim.bo.shiftwidth = 2
+    vim.bo.tabstop = 2
+    vim.bo.softtabstop = 2
+    vim.bo.expandtab = true
+  end,
+})
+
